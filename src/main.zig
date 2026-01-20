ecbcb82c5feabf5ab8d2140807c44c49fed8a825
phonaputer <njerome@uwalumni.com>
2026-01-20 21:15:11 +0900

    Downgrade to Zig 0.15.2 since it seems 0.16.0 is not super stable. Add input reading to Hello World.

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,13 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("Hello world!\n", .{});
+    var stdin_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&.{});
+    var stdin_reader = std.fs.File.stdin().reader(&stdin_buffer);
+
+    try stdout_writer.interface.print("Please enter a dice algebra expression: ", .{});
+
+    const roll = try stdin_reader.interface.takeDelimiterExclusive('\n');
+
+    try stdout_writer.interface.print("You wrote: {s}\n", .{roll});
 }
